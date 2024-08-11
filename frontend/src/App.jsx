b25a851090ce8df2fa306bc57c368a1795807609
Md Tabraiz <mohammadtabraiz3@gmail.com>
2024-08-11 21:53:20 +0530

    final code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
     setBanner(response.data);
     setTimeLeft(response.data.timer);
   };
-  // console.log(banner)
+ 
 
   useEffect(() => {
     fetchBanner();
--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -2,58 +2,62 @@ import React, { useState, useEffect } from "react";
 import axios, { isCancel, AxiosError } from "axios";
 
 const Dashboard = (props) => {
-  const { banner, fetchBanner } = props;
-  const initials = {
-    description: banner.description,
-    timer: banner.timer,
-    link: banner.link,
-    isVisible: banner.isVisible,
-  };
-  const [banners, setBanners] = useState(initials);
+  const { banner } = props;
+
+  const [data, setData] = useState({
+    description: banner.description || "",
+    timer: banner.timer || "",
+    link: banner.link || "",
+    isVisible: banner.isVisible || false,
+  });
+
+  useEffect(() => {
+    if (banner) {
+      setData({
+        description: banner.description || "",
+        timer: banner.timer || "",
+        link: banner.link || "",
+        isVisible: banner.isVisible || false,
+      });
+    }
+  }, [banner]);
   const updateBanner = async () => {
-    await axios.put(
-      "https://takeuforward-igf3.onrender.com/api/banner",
-      banners
-    );
+    await axios.put("https://takeuforward-igf3.onrender.com/api/banner", data);
     fetchBanner();
   };
-
+  console.log(data);
   return (
     <div className="dashboard">
       <label>
         Banner Description:
         <input
           type="text"
-          value={banners.description}
-          onChange={(e) =>
-            setBanners({ ...banners, description: e.target.value })
-          }
+          value={data.description}
+          onChange={(e) => setData({ ...data, description: e.target.value })}
         />
       </label>
       <label>
         Timer (in seconds):
         <input
           type="number"
-          value={banners.timer}
-          onChange={(e) => setBanners({ ...banners, timer: e.target.value })}
+          value={data.timer}
+          onChange={(e) => setData({ ...data, timer: e.target.value })}
         />
       </label>
       <label>
         Banner Link:
         <input
           type="text"
-          value={banners.link}
-          onChange={(e) => setBanners({ ...banners, link: e.target.value })}
+          value={data.link}
+          onChange={(e) => setData({ ...data, link: e.target.value })}
         />
       </label>
       <label>
         Banner Visible:
         <input
           type="checkbox"
-          checked={banners.isVisible}
-          onChange={(e) =>
-            setBanners({ ...banners, isVisible: e.target.checked })
-          }
+          checked={data.isVisible}
+          onChange={(e) => setData({ ...data, isVisible: e.target.checked })}
         />
       </label>
       <button onClick={updateBanner}>Update Banner</button>
